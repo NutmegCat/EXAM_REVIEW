7860fe6c5922f843e2b44e41a1a5d1e7dda83fb2
NutmegCat <aaronprince.anu@student.tdsb.on.ca>
2023-06-17 11:56:22 -0400

    1.2

--- exam.java
+++ exam.java
@@ -1,7 +1,24 @@
 /* This is an exam review for ICS3U1 */
 
-class exam{
+class exam{ // All Java programs are contained in a class. The name *exam* is the name of the class.
     public static void main(String[] args) {
-        
+        firstProgram();
+        advice();
+        escapeCharacters();
+    }
+
+    public static void firstProgram() {
+        System.out.println("\nHello, World!"); // This is a statement. This prints out "Hello, World!"
+    }
+
+    public static void advice() {
+        System.out.print("\nIf at first ");
+        System.out.println("you don't succeed" + ","); // The plus indicates a concatenation.
+        System.out.print("failure may be ");
+        System.out.println("your style");
+    }
+
+    public static void escapeCharacters() {
+        System.out.println("\nA backslash is \\\nand a double quote is \"."); // The backslash indicates an escape character.
     }
 }
