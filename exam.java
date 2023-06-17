ecfd7f42c0f7b13efd3a8f828846094979f132c9
NutmegCat <aaronprince.anu@student.tdsb.on.ca>
2023-06-17 13:55:01 -0400

    what position is a letter in the alphabet

--- exam.java
+++ exam.java
@@ -2,6 +2,13 @@
 
 class exam{ // All Java programs are contained in a class. The name *exam* is the name of the class.
     public static void main(String[] args) {
-        
+        System.out.println("Please enter your letter: ");
+        System.out.print("  >  ");
+        char c = In.getChar();
+
+        int position = c - 64; // 64 is the ASCII value of 'A'
+
+        System.out.println();
+        System.out.println(c + " is at position " + position + " in the alphabet.");
     }
 }
